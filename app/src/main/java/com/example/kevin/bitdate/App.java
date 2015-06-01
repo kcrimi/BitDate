76b6be46e5ef5273e015fe2e6e3ff8f3558c7ca8
kcrimi <kevincrimi@gmail.com>
2015-06-01 16:59:00 -0700

    intefrate facebook with parse

--- app/src/main/java/com/example/kevin/bitdate/App.java
+++ app/src/main/java/com/example/kevin/bitdate/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 
 /**
  * Created by kevin on 6/1/15.
@@ -20,6 +21,6 @@ public class App extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "rgHspdCFiUuKeRqA6vtNjtGK1pLx6EtiuYAgyzgo", "jhzqFwzuqZN7xxk0LW4rFkQN37VRY0OCrE4XYEBa");
-
+        ParseFacebookUtils.initialize(this);
     }
 }
