c3be39b9e84e17acc5fbf5137500db884945aa4c
kcrimi <kevincrimi@gmail.com>
2015-06-02 13:49:50 -0700

    add in Action Data source

---
+++ app/src/main/java/com/example/kevin/bitdate/ActionDataSource.java
@@ -0,0 +1,16 @@
+package com.example.kevin.bitdate;
+
+/**
+ * Created by kevin on 6/2/15.
+ */
+public class ActionDataSource {
+
+    private static final String TABLE_NAME = "Action";
+    private static final String COLUMN_BY_USER = "firstName";
+    private static final String COLUMN_TO_USER = "firstName";
+    private static final String COLUMN_TYPE = "type";
+
+    private static final String TYPE_LIKED = "liked";
+    private static final String TYPE_MATCHED = "matched";
+    private static final String TYPE_SKIPPED = "skipped";
+}
