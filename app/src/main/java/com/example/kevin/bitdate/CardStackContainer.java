06841c933e4a98fbb190982b44f950c34251b24f
kcrimi <kevincrimi@gmail.com>
2015-06-01 20:03:15 -0700

    dragable cards

--- app/src/main/java/com/example/kevin/bitdate/CardStackContainer.java
+++ app/src/main/java/com/example/kevin/bitdate/CardStackContainer.java
@@ -1,13 +1,24 @@
 package com.example.kevin.bitdate;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 /**
  * Created by kevin on 6/1/15.
  */
-public class CardStackContainer extends RelativeLayout {
+public class CardStackContainer extends RelativeLayout implements View.OnTouchListener {
+
+    private static final String TAG = "Card Stack Container";
+    private CardAdapter mAdapter;
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private float mPositionX;
+    private float mPositionY;
 
     public CardStackContainer(Context context) {
         this(context, null, 0);
@@ -19,5 +30,46 @@ public class CardStackContainer extends RelativeLayout {
 
     public CardStackContainer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void setAdapter(CardAdapter adapter) {
+        mAdapter = adapter;
+        if (mAdapter.getCount() > 0) {
+            CardView cardView = mAdapter.getView(0, null, this);
+            cardView.setOnTouchListener(this);
+            addView(cardView);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                mLastTouchX = event.getX();
+                mLastTouchY = event.getY();
+
+                mPositionX = v.getX();
+                mPositionY = v.getY();
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float xMove = event.getX();
+                float yMove = event.getY();
+
+                float changeX = xMove - mLastTouchX;
+                float changeY = yMove - mLastTouchY;
+
+                mPositionX += changeX;
+                mPositionY += changeY;
+
+                v.setX(mPositionX);
+                v.setY(mPositionY);
+
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/kevin/bitdate/ChoosingFragment.java
+++ app/src/main/java/com/example/kevin/bitdate/ChoosingFragment.java
@@ -6,19 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ChoosingFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ChoosingFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+        CardStackContainer cardStack = (CardStackContainer)v.findViewById(R.id.card_stack);
+        User user = new User();
+        user.setFirstName("Kevin");
+        List<User> users = new ArrayList<User>();
+        users.add(user);
+        CardAdapter cardAdapter = new CardAdapter(getActivity(), users);
+        cardStack.setAdapter(cardAdapter);
         return v;
     }
 }
